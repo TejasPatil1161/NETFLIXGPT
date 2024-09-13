e6f39f5104a6016794d3b6e41096e773a399f991
Anshul Kasana <anshul.kasana98@gmail.com>
2024-09-14 01:51:39 +0530

    Sign in/up and validation checks

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NETFLIX_LOGO } from "../utils/constants";
 
 const Header = () => {
   return (
-    <div className="w-full lg:px-12 md:px-8 px-4 py-6 m-auto">
+    <div className="w-full lg:px-12 md:px-8 px-4 py-6 bg-black md:bg-transparent">
       <img src={NETFLIX_LOGO} alt="logo" className="md:w-44 w-36" />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,130 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { LOGIN_BG_IMAGE } from "../utils/constants";
 import Header from "./Header";
+import { checkValidation } from "../utils/validate";
+
+const ErrorMessage = ({ message }) => {
+  return (
+    <div className="flex flex-col gap-1">
+      {message.map((err, i) => {
+        return (
+          <p
+            key={i}
+            className="text-xs md:text-sm flex justify-start items-center text-red-500 font-medium"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+              />
+            </svg>
+            <span>{err}</span>
+          </p>
+        );
+      })}
+    </div>
+  );
+};
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+  const email = useRef(null);
+  const password = useRef(null);
+  const confirmPassword = useRef(null);
 
   const toggleSignin = () => {
     setIsSignIn(!isSignIn);
   };
 
+  const handleSubmit = () => {
+    // Check validations
+    const error = checkValidation(
+      email?.current.value,
+      password?.current.value
+    );
+
+    let errors = {
+      email: error.emailCheck,
+      password: error.passwordCheck,
+    };
+
+    if (!isSignIn) {
+      if (password?.current.value !== confirmPassword?.current.value) {
+        errors = {
+          ...errors,
+          confirmPassword: "Passwords don't match",
+        };
+      }
+    }
+
+    setErrorMessage(errors);
+  };
+
   return (
     <div
-      className="h-screen flex flex-col"
+      className="min-h-screen"
       style={{ backgroundImage: `url(${LOGIN_BG_IMAGE})` }}
     >
-      <div className="absolute inset-0 bg-black h-screen opacity-45 z-10"></div>
-      <div className="flex flex-col w-full relative z-20 flex-grow">
+      <div className="relative flex flex-col min-h-screen w-full z-10 bg-black md:bg-transparent">
         <Header />
-        <div className="flex w-full h-full justify-center items-start">
-          <form className="text-white flex flex-col md:w-3/12 bg-black bg-opacity-75 p-14 rounded-md">
+        <div className="flex w-full md:h-full justify-start md:justify-center items-start px-7">
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="text-white flex flex-col lg:w-1/3 w-full bg-black bg-opacity-75 md:p-14 rounded-md"
+          >
             <h1 className="text-4xl font-bold mb-5">
               {isSignIn ? "Sign In" : "Sign Up"}
             </h1>
             {!isSignIn && (
               <input
                 type="text"
-                placeholder="Name"
+                placeholder="Name*"
                 className="p-4 my-2 bg-transparent outline-none border border-stone-400 rounded-md placeholder:text-stone-400 placeholder:text-lg placeholder:font-medium"
               />
             )}
             <input
+              ref={email}
               type="text"
-              placeholder="Email"
+              placeholder="Email*"
               className="p-4 my-2 bg-transparent outline-none border border-stone-400 rounded-md placeholder:text-stone-400 placeholder:text-lg placeholder:font-medium"
             />
+            {errorMessage?.email && (
+              <ErrorMessage message={[errorMessage?.email]} />
+            )}
             <input
+              ref={password}
               type="password"
-              placeholder="Password"
+              placeholder="Password*"
               className="p-4 my-2 bg-transparent outline-none border border-stone-400 rounded-md placeholder:text-stone-400 placeholder:text-lg placeholder:font-medium"
             />
-            <button className="p-2 my-2 bg-red-600 font-semibold text-lg rounded-md hover:bg-red-700">
-              Sign In
+            {errorMessage?.password && (
+              <ErrorMessage message={errorMessage?.password} />
+            )}
+            {!isSignIn && (
+              <input
+                ref={confirmPassword}
+                type="password"
+                placeholder="Confirm Password*"
+                className="p-4 my-2 bg-transparent outline-none border border-stone-400 rounded-md placeholder:text-stone-400 placeholder:text-lg placeholder:font-medium"
+              />
+            )}
+            {errorMessage?.password && errorMessage?.confirmPassword && (
+              <ErrorMessage message={[errorMessage?.confirmPassword]} />
+            )}
+            <button
+              onClick={handleSubmit}
+              className="p-2 my-2 bg-red-600 font-semibold text-lg rounded-md hover:bg-red-700"
+            >
+              {isSignIn ? "Sign In" : "Sign Up"}
             </button>
             <p className="my-2 text-gray-300 font-medium text-lg">
               {isSignIn ? "New to Netflix? " : "Already a user? "}
@@ -54,6 +138,7 @@ const Login = () => {
           </form>
         </div>
       </div>
+      <div className="absolute inset-0 bg-black opacity-45"></div>
     </div>
   );
 };
