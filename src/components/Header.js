86cb2c30099e2746b1f33d45418e3e2b06528c38
Anshul Kasana <anshul.kasana98@gmail.com>
2024-09-19 11:18:49 +0530

    Added firebase Authentication for email and google as well as added Redirecting to browse page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,37 @@
 import React from "react";
 import { NETFLIX_LOGO } from "../utils/constants";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { doSignout } from "../utils/authenticate";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+
+  const handleSignOut = async () => {
+    try {
+      await doSignout();
+      navigate("/");
+    } catch (error) {
+      console.log("An error occured while Signing out:", error);
+    }
+  };
+
   return (
-    <div className="w-full lg:px-12 md:px-8 px-4 py-6 bg-black md:bg-transparent">
+    <div className="w-full lg:px-12 md:px-8 px-4 py-6 bg-black md:bg-transparent flex justify-between">
       <img src={NETFLIX_LOGO} alt="logo" className="md:w-44 w-36" />
+      {user && (
+        <div className="flex justify-center items-center">
+          <img
+            src={user?.photoURL}
+            alt="avatar"
+            className="w-12 h-12 rounded-md"
+          />
+          <button className="ml-2" onClick={handleSignOut}>
+            Sign out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/utils/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFaCVgGiqJ0x0153eDey4Z0IkBMc0PIGI",
+  authDomain: "netflixgpt-81199.firebaseapp.com",
+  projectId: "netflixgpt-81199",
+  storageBucket: "netflixgpt-81199.appspot.com",
+  messagingSenderId: "853877822339",
+  appId: "1:853877822339:web:01fd3161adf86b8c385cb6",
+  measurementId: "G-EMD6JZZKN7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export { app, auth };
